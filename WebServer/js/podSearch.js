354a0356ddac1574a9599a185a26c1b313461be1
Xingdong Zhang <xzhang@esri.com>
2015-04-29 09:33:44 -0700

    Bug fix for Pod Search widget
    Restrict maxSuggestions workaround only to feature layers that doesn't
    support pagination.

--- WebServer/js/podSearch.js
+++ WebServer/js/podSearch.js
@@ -1,60 +1,62 @@
 define([
     "dojo/_base/declare",
     "dojo/_base/array",
-	"dojo/_base/lang",
-	"dojo/i18n!./nls/podi18n",
-	"esri/map",
-	"esri/dijit/Search",
-	"esri/layers/FeatureLayer",
-	"esri/InfoTemplate",
-	"esri/SpatialReference",
-	"esri/geometry/Extent",
-	"esri/tasks/locator",
-	"dojo/domReady!"
-], function (declare, array, lang, i18n, Map, EsriSearch, FeatureLayer, InfoTemplate, SpatialReference, Extent, Locator) {
-    var maxSuggestionCount = 20;
+    "dojo/_base/lang",
+    "dojo/i18n!./nls/podi18n",
+    "esri/map",
+    "esri/dijit/Search",
+    "esri/layers/FeatureLayer",
+    "esri/InfoTemplate",
+    "esri/SpatialReference",
+    "esri/geometry/Extent",
+    "esri/tasks/locator",
+    "dojo/domReady!"
+], function(declare, array, lang, i18n, Map, EsriSearch, FeatureLayer, InfoTemplate, SpatialReference, Extent, Locator) {
     return declare("esri.production.dijit.Search", [EsriSearch], {
         sourceGeocodes: [],
         sourceLayers: [],
         esriMap: null,
 
-        setSearchLayers: function (sourceLayers) {
+        setSearchLayers: function(sourceLayers) {
             var sources = [];
 
-            array.forEach(sourceLayers, function (sourceLayer) {
+            array.forEach(sourceLayers, function(sourceLayer) {
 
                 var infoFieldsTemplate = sourceLayer.infoTemplate;
                 if (infoFieldsTemplate === undefined) {
                     var infoFields = [sourceLayer.displayFiled];
                     for (var i = 0; i < sourceLayer.searchFields.length; i++) {
-                        if (sourceLayer.searchFields[i] != '*' && sourceLayer.searchFields[i] != sourceLayer.displayFiled) {
+                        if (sourceLayer.searchFields[i] != "*" && sourceLayer.searchFields[i] != sourceLayer.displayFiled) {
                             infoFields.push(sourceLayer.searchFields[i]);
                         }
                     }
-                    infoFieldsTemplate = '';
-                    for (var i = 0; i < infoFields.length; i++) {
-                        infoFieldsTemplate += infoFields[i].toString() + ": ${" + infoFields[i] + "}</br>"
+                    infoFieldsTemplate = "";
+                    for (i = 0; i < infoFields.length; i++) {
+                        infoFieldsTemplate += infoFields[i].toString() + ": ${" + infoFields[i] + "}</br>";
                     }
                 }
 
-                sources.push(
-					  {
-					      featureLayer: new FeatureLayer(sourceLayer.url),
-					      searchFields: sourceLayer.searchFields,
-					      displayField: sourceLayer.displayFiled,
-					      exactMatch: false,
-					      outFields: sourceLayer.searchFields,
-					      name: sourceLayer.title,
-					      placeholder: i18n.search.searchHint,
-					      //Fix "Pagination is not supported"
-					      maxResults: 'somestring',
-					      maxSuggestions: 'somestring',
-					      infoTemplate: new InfoTemplate(sourceLayer.title, infoFieldsTemplate),
-					      enableSuggestions: true,
-					      minCharacters: 0
-					  });
+                sources.push({
+                    featureLayer: new FeatureLayer(sourceLayer.url),
+                    searchFields: sourceLayer.searchFields,
+                    displayField: sourceLayer.displayFiled,
+                    exactMatch: false,
+                    outFields: sourceLayer.searchFields,
+                    name: sourceLayer.title,
+                    placeholder: i18n.search.searchHint,
+                    infoTemplate: new InfoTemplate(sourceLayer.title, infoFieldsTemplate),
+                    enableSuggestions: true,
+                    minCharacters: 0
+                });
+
+                // Fix "Pagination is not supported"
+                var featureLayer = sources[sources.length-1].featureLayer;
+                if (featureLayer.queryPagination === false) {
+                    sources[sources.length-1].maxSuggestions = "somestring";
+                    sources[sources.length-1].maxResults = "somestring";
+                }
             });
-            array.forEach(this.sourceGeocodes, function (sourceGeocode) {
+            array.forEach(this.sourceGeocodes, function(sourceGeocode) {
                 sources.push({
                     locator: new Locator(sourceGeocode.url),
                     singleLineFieldName: "SingleLine",
@@ -64,25 +66,25 @@ define([
                         distance: 50000
                     },
                     placeholder: i18n.search.searchHint,
-                    maxResults: maxSuggestionCount,
-                    maxSuggestions: maxSuggestionCount,
                     enableSuggestions: true,
                     minCharacters: 0
-                })
+                });
             });
             this.set("sources", sources);
         },
 
-        getSources: function () { return this.get("sources"); },
+        getSources: function() {
+            return this.get("sources");
+        },
 
-        updateSources: function () {
+        updateSources: function() {
             var sourceLayersOnMap = this.getSourceLayersOnMap();
             if (this.areLayersActual(sourceLayersOnMap))
                 return;
             this.setSearchLayers(sourceLayersOnMap);
         },
 
-        getSourceLayersOnMap: function () {
+        getSourceLayersOnMap: function() {
             var sourceLayersOnMap = [];
             for (var i = 0; i < this.sourceLayers.length; i++) {
                 var sourceLayer = this.sourceLayers[i];
@@ -92,17 +94,17 @@ define([
             return sourceLayersOnMap;
         },
 
-        getLocatorCount: function () {
+        getLocatorCount: function() {
             var locatorCount = 0;
             var sources = this.getSources();
-            array.forEach(sources, function (source) {
+            array.forEach(sources, function(source) {
                 if (source.locator !== undefined)
                     locatorCount++;
             });
             return locatorCount;
         },
 
-        areLayersActual: function (infoLayers) {
+        areLayersActual: function(infoLayers) {
             var sources = this.getSources();
             if (infoLayers.length != sources.length - this.getLocatorCount())
                 return false;
@@ -123,38 +125,46 @@ define([
             return isActualSourceLayer;
         },
 
-        addSourceLayer: function (sourceLayer) {
+        addSourceLayer: function(sourceLayer) {
             this.sourceLayers.push(sourceLayer);
             this.updateSources();
         },
 
-        startup: function (sourceLayers, sourceGeocodes) {
+        startup: function(sourceLayers, sourceGeocodes) {
             this.inherited(arguments);
             if (sourceLayers !== undefined)
                 this.sourceLayers = sourceLayers;
             if (sourceGeocodes !== undefined)
                 this.sourceGeocodes = sourceGeocodes;
             this.updateSources();
-            this.map.on("layer-remove", lang.hitch(this, function () { this.updateSources(); }));
-            this.map.on("layer-add", lang.hitch(this, function () { this.updateSources(); }));
+            this.map.on("layer-remove", lang.hitch(this, function() {
+                this.updateSources();
+            }));
+            this.map.on("layer-add", lang.hitch(this, function() {
+                this.updateSources();
+            }));
         },
 
-        _supportsPagination: function (source) {
+        _supportsPagination: function(source) {
             for (var i = 0; i < this.sources.length; i++) {
                 //Fix "Pagination is not supported"
-                this.sources[i].maxSuggestions = "somestring";
+                var featureLayer = this.sources[i].featureLayer;
+                if (featureLayer !== undefined && featureLayer.advancedQueryCapabilities.supportsPagination === false) {
+                    this.sources[i].maxSuggestions = "somestring";
+                }
             }
             return true;
         },
 
-        _insertSuggestions: function (suggestions) {
+        _insertSuggestions: function(suggestions) {
             for (var i = 0; i < this.sources.length; i++) {
-                this.sources[i].maxSuggestions = this.maxSuggestions;
+                var featureLayer = this.sources[i].featureLayer;
+                if (featureLayer !== undefined && featureLayer.advancedQueryCapabilities.supportsPagination === false) {
+                    this.sources[i].maxSuggestions = this.maxSuggestions;;
+                }
             }
             this.inherited(arguments);
         }
     });
 
 });
-
-
